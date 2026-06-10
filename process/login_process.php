273453ba886733f8fbba1c79f24467ad24e36de2
abyanhrf <abyanhrf@gmail.com>
2026-06-10 12:17:33 +0700

    penambahan login feature

---
+++ process/login_process.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('../config/database.php');
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email = ?";
+
+$stmt = mysqli_prepare($conn,$sql);
+mysqli_stmt_bind_param($stmt, "s", $email);
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+
+$user = mysqli_fetch_assoc($result);
+
+if ($user) {
+    if (password_verify($password, $user['password'])) {
+        
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['nama'] = $user['nama'];
+        $_SESSION['role'] = $user['role'];
+
+        header("Location: ../home/home.php");
+        exit;
+    }
+
+    else {
+        echo "Password salah!";
+    }
+} else {
+    echo "Email tidak ditemukan!";
+}
