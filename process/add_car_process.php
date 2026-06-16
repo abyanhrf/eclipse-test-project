fdcbe0abe5adbc57a8d48c98bb84d673e3b0ddb1
abyanhrf <abyanhrf@gmail.com>
2026-06-16 15:09:17 +0700

    penambahan kolom database car

--- process/add_car_process.php
+++ process/add_car_process.php
@@ -7,58 +7,69 @@ $tipe_mobil = $_POST['tipe_mobil'];
 $harga = $_POST['harga'];
 $tahun = $_POST['tahun'];
 $stok = $_POST['stok'];
+$bahan_bakar = $_POST['bahan_bakar'];
+$kapasitas_mesin = $_POST['kapasitas_mesin'];
+$transmisi = $_POST['transmisi'];
+$warna = $_POST['warna'];
+$kilometer = $_POST['kilometer'];
 $deskripsi = $_POST['deskripsi'];
 
-$gambar = $_FILES['gambar'];
-
-$namaFile = time() . "_" . basename($gambar['name']);
-
-$target = "../uploads/" . $namaFile;
-
-if ($gambar['error'] != 0) {
-    die("Upload gambar gagal");
-}
-
-if (!move_uploaded_file(
-    $gambar['tmp_name'],
-    $target
-)) {
-    die("Gagal menyimpan gambar");
-}
-
-$sql ="INSERT INTO cars (nama_mobil, merek, tipe_mobil, harga, tahun, stok, deskripsi) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$sql ="INSERT INTO cars (nama_mobil, merek, tipe_mobil, harga, tahun, stok, bahan_bakar, kapasitas_mesin, transmisi, warna, kilometer, deskripsi)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 $stmt = mysqli_prepare($conn, $sql);
 
 mysqli_stmt_bind_param(
     $stmt,
-    "sssiiis",
+    "sssiiissssis",
     $nama_mobil,
     $merek,
     $tipe_mobil,
     $harga,
     $tahun,
     $stok,
-    $deskripsi
+    $bahan_bakar,
+    $kapasitas_mesin,
+    $transmisi,
+    $warna,
+    $kilometer,
+    $deskripsi,
 );
 
 if (mysqli_stmt_execute($stmt)) {
 
     $car_id = mysqli_insert_id($conn);
 
-    $sqlImg = "INSERT INTO car_img (car_id, gambar)
-            VALUES (?, ?)";
+    $x=count($_FILES['gambar']['name']);
 
-    $stmtImg = mysqli_prepare($conn, $sqlImg);
+    for ($i = 0; $i < $x; $i++) {
+        $namaFile = time() . "_" . $i . "_" . basename($_FILES['gambar']['name'][$i]);
 
-    mysqli_stmt_bind_param(
-        $stmtImg,
-        "is",
-        $car_id,
-        $namaFile
-    );
+    $target = "../uploads/" . $namaFile;
 
-    mysqli_stmt_execute($stmtImg);
+    if (move_uploaded_file(
+        $_FILES['gambar']['tmp_name'][$i],
+        $target
+        )
+    ) {
+        $gambar_utama = ($i == 0) ? 1 : 0;
+
+        $sqlImg = "INSERT INTO cars_img (car_id, gambar, gambar_utama)
+        VALUES (?, ?, ?)";
+
+        $stmtImg = mysqli_prepare($conn, $sqlImg);
+
+        mysqli_stmt_bind_param(
+            $stmtImg,
+            "isi",
+            $car_id,
+            $namaFile,
+            $gambar_utama
+        );
+
+        mysqli_stmt_execute($stmtImg);
+        }
+    }
 
     header("Location: ../dashboard/dashbarang.php");
     exit;
