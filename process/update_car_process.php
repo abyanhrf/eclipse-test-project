138f766be817811288862b7d23e840f6a7f8b0fa
abyanhrf <abyanhrf@gmail.com>
2026-06-26 02:04:08 +0700

    max 5 img update

--- process/update_car_process.php
+++ process/update_car_process.php
@@ -48,28 +48,34 @@
     if (mysqli_stmt_execute($stmt)) {
 
     if (!empty($_FILES['gambar_baru']['name'][0])) {
-        
+
+    // 1. Hitung dulu berapa gambar yang SUDAH ADA di database untuk mobil ini
+    $sqlCount = "SELECT COUNT(*) AS total FROM cars_img WHERE car_id = ?";
+    $stmtCount = mysqli_prepare($conn, $sqlCount);
+    mysqli_stmt_bind_param($stmtCount, "i", $id);
+    mysqli_stmt_execute($stmtCount);
+    $resultCount = mysqli_stmt_get_result($stmtCount);
+    $rowCount = mysqli_fetch_assoc($resultCount);
+    $jumlah_sekarang = $rowCount['total'];
+
+    $sisa_slot = 5 - $jumlah_sekarang;
+
+    // 2. Proses upload HANYA jika masih ada sisa slot
+    if ($sisa_slot > 0) {
         $x = count($_FILES['gambar_baru']['name']);
-        
-        for ($i = 0; $i < $x; $i++) { 
+        $batas_maksimal = min($x, $sisa_slot); // Jangan melebihi sisa slot
+
+        for ($i = 0; $i < $batas_maksimal; $i++) { 
             $namaFile = time() . "_" . $i . "_" . basename($_FILES['gambar_baru']['name'][$i]);
             $target = "../uploads/" . $namaFile;
-            
+
             if (move_uploaded_file($_FILES['gambar_baru']['tmp_name'][$i],$target)) {
                 $gambar_utama = 0;
-                $sqlImg = "INSERT INTO cars_img (car_id, gambar,gambar_utama) VALUES (?, ?, ?)";
-
+                $sqlImg = "INSERT INTO cars_img (car_id, gambar, gambar_utama) VALUES (?, ?, ?)";
                 $stmtImg = mysqli_prepare($conn, $sqlImg);
-
-                mysqli_stmt_bind_param(
-                $stmtImg,
-                "isi",
-                $id,
-                $namaFile,
-                $gambar_utama
-                );
-
+                mysqli_stmt_bind_param($stmtImg, "isi", $id, $namaFile, $gambar_utama);
                 mysqli_stmt_execute($stmtImg);
+                }
             }
         }
     }
